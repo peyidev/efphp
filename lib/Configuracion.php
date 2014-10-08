1f6be4c3c57c5069047256d080a6ab78f3975ae7
peyidev <peyi.god@gmail.com>
2014-10-07 23:40:44 -0500

    Update Configuracion.php

--- lib/Configuracion.php
+++ lib/Configuracion.php
@@ -10,10 +10,10 @@ define('DB_PASSWORD', '');
 
 #Paypal config
 
-define('USR','peyi.god_api1.gmail.com');
-define('PWD','7X7C8EBP6H9PW7NP');
-define('SIGNATURE','AursJbKtSlFmEssFHjEY6Op1BWMzAxT2mV.XsRjAn8YybSxIy9Jv2WLM');
-define('PAYPALURL','https://api-3t.sandbox.paypal.com/nvp');
+define('USR','');
+define('PWD','');
+define('SIGNATURE','');
+define('PAYPALURL','');
 
 #Global config
 define('ADMINURL', 'admin/');
